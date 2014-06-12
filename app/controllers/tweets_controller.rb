994349c9a282a52a4d9a13b8cfccb505b4062dad
Bev <beverlymah@gmail.com>
2014-06-12 18:20:24 -0400

    add fetch_tweets in truck model

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 	
 	def index
+		
 
 	end
 
--- app/models/truck.rb
+++ app/models/truck.rb
@@ -4,4 +4,13 @@ class Truck < ActiveRecord::Base
 
 	validates :name, presence: true
 	validates :twitter_handle, uniqueness: true
+
+
+	def fetch_tweets!
+		trucks_tweets = CLIENT.user_timeline(self.username, count: 5)
+		trucks_tweets.each do |tweet|
+			Tweet.create(body: tweet.tweet, tweet_time: tweet.created_at)
+		end
+	end
+
 end

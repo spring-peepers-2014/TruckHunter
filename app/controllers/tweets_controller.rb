c627ff47e906db6bbd089a86bb77c008cade308c
Bev <beverlymah@gmail.com>
2014-06-12 17:00:00 -0400

    add tweets controller

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,5 @@
+class TweetsController < ApplicationController
+
+
+
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
 
 
   CLIENT = Twitter::REST::Client.new do |config|
-  config.consumer_key = ENV['CONSUMER_KEY']
-  config.consumer_secret = ENV['CONSUMER_SECRET']
+    config.consumer_key = ENV['CONSUMER_KEY']
+    config.consumer_secret = ENV['CONSUMER_SECRET']
  end
- 
+
 end

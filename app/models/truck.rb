c419ca534edef8ee00d94353fc9e79b03a85ced7
Isabel Rial <isabel.rial@gmail.com>
2014-06-13 10:06:30 -0400

    update tweet migration column name

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -7,11 +7,14 @@ class Truck < ActiveRecord::Base
 
 
 	def fetch_tweets!
-		trucks_tweets = CLIENT.user_timeline(self.twitter_handle, count: 5)
-		p trucks_tweets
-		
+		trucks_tweets = CLIENT.user_timeline(self.twitter_handle, count: 5, exclude_replies: true)
+		# p trucks_tweets.text
+
+		# p Geocoder.search("21st and 5th from 9:00-3:00 new york").first
+
 		trucks_tweets.each do |tweet|
-			Tweet.create(body: tweet.tweet, tweet_time: tweet.created_at)
+			p tweet.to_json
+			# Tweet.create(body: tweet.tweet, tweet_time: tweet.created_at)
 		end
 	end
 

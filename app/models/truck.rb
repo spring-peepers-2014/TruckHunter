daf80b5c67dea9233f37563c90cd3595d80ecaf0
Isabel Rial <isabel.rial@gmail.com>
2014-06-13 11:48:50 -0400

    update truck model to check recency of location data and geocode coordinates

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -10,17 +10,33 @@ class Truck < ActiveRecord::Base
 		trucks_tweets = CLIENT.user_timeline(self.twitter_handle, count: 5, exclude_replies: true)
 		# p trucks_tweets.text
 
-		# p Geocoder.search("21st and 5th new york").first
 
 		trucks_tweets.each do |tweet|
 
-			p "Geo enabled:" JSON.parse(tweet.to_json)["user"]["geo_enabled"]
+			p JSON.parse(tweet.to_json)["user"]["geo_enabled"]
 			# Tweet.create(body: tweet.tweet, tweet_time: tweet.created_at)
 		end
 	end
 
-	def has_current_tweets?
-		# @tweets = self.tweets.last
+	def has_current_location?
+		return false if self.latitude.nil? || self.longitude.nil?
+		time_since_update = Time.now - self.updated_at
+
+		if time_since_update > 14400 
+			false
+		else
+			true
+		end
+	end
+
+
+	def geocode_coordinates(location)
+		geo_data = Geocoder.search(location).first
+		latitude = geo_data.geometry["location"]["lat"]
+		longitude = geo_data.geometry["location"]["lng"]
+
+		self.latitude = latitude
+		self.longitude = longitude
 	end
 
 

7934148cc889f8b8380d86c7630c739f2527063f
Insung Lee <inslee0921@gmail.com>
2014-06-13 12:22:23 -0400

    add initial map, and starting to work on ajax

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,7 +1,23 @@
 class TrucksController < ApplicationController
+  respond_to :json
 
   def index
-
+    "you"
+    @geojson = []
+    @geojson << {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: ["73.9857", "40.7484"]
+    },
+    properties: {
+      name: "Nirav shit",
+      address: "34 st",
+      :'marker-color' => '#00607d',
+      :'marker-symbol' => 'circle',
+      :'marker-size' => 'medium'
+    }
+  }
   end
 
 

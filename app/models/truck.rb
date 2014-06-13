d85191cd9eff10eb51d2e48c55678558949a6853
Nirav Chheda <niravchheda@gmail.com>
2014-06-13 15:25:16 -0400

    uncommented tweet stuff on model before heroku push

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -6,23 +6,23 @@ class Truck < ActiveRecord::Base
 	validates :twitter_handle, uniqueness: true
 
 
-	# def fetch_tweets!
-	# 	trucks_tweets = CLIENT.user_timeline(self.twitter_handle, count: 5, exclude_replies: true)
-	# 	# p trucks_tweets.text
+	def fetch_tweets!
+		trucks_tweets = CLIENT.user_timeline(self.twitter_handle, count: 5, exclude_replies: true)
+		# p trucks_tweets.text
 
-	# 	# p Geocoder.search("21st and 5th new york").first
+		# p Geocoder.search("21st and 5th new york").first
 
-	# 	trucks_tweets.each do |tweet|
+		trucks_tweets.each do |tweet|
 
-	# 		# p "Geo enabled:" JSON.parse(tweet.to_json)["user"]["geo_enabled"]
-	# 		# Tweet.create(body: tweet.tweet, tweet_time: tweet.created_at)
-	# 	end
+			# p "Geo enabled:" JSON.parse(tweet.to_json)["user"]["geo_enabled"]
+			# Tweet.create(body: tweet.tweet, tweet_time: tweet.created_at)
+		end
 		
-	# end
+	end
 
-	# def has_current_tweets?
-	# 	# @tweets = self.tweets.last
-	# end
+	def has_current_tweets?
+		# @tweets = self.tweets.last
+	end
 
 
 end

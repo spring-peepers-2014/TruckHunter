ecf592fec27875bbe0223833710fb42290f6d157
Isabel Rial <isabel.rial@gmail.com>
2014-06-13 18:19:55 -0400

    revert to twitter rest api

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 
 
 
-  CLIENT = Twitter::Streaming::Client.new do |config|
+  CLIENT = Twitter::REST::Client.new do |config|
     config.consumer_key = ENV['CONSUMER_KEY']
     config.consumer_secret = ENV['CONSUMER_SECRET']
  end

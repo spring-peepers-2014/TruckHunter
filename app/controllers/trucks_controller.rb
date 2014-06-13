bef67dbb0bc3bf082ee96981ecbc34e7f26d4c69
Isabel Rial <isabel.rial@gmail.com>
2014-06-13 15:30:10 -0400

    delete whitespace in trucks controller

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -4,8 +4,6 @@
 		@trucks = Truck.where(approved: true, active: true)
 		@current_trucks = @trucks.select { |truck| truck.has_current_location? }
 		@unknown_trucks = @trucks - @current_trucks
-
-		
 	end
 
 

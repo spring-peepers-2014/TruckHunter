1f7947863da8a863efe39a5f40b1e38a9864e2bf
niravchheda <niravchheda@gmail.com>
2014-06-14 14:53:52 -0400

    separated concerns of tweet parsing

--- app/models/location_hunter.rb
+++ app/models/location_hunter.rb
@@ -1,5 +1,42 @@
-class LocationHunter
+module LocationHunter
 
-	
+	def parse_tweet(tweet)
+		my_match = /(@|at|on)\s+((?:\S+\s)?\S*(and|&)\S*(?:\s\S+)?)|\S\d+\s\b\w+\b\s(Avenue|Ave|Street|St)|\A?^?\d+\s(\b\w+\b\s)+(Avenue|Ave|Street|St)|(\b\w+\b\s){2}Park|(\b\w+\b\s)(St|Street)\sand?\s(\b\w+\b\s)(St|Street)/i.match(tweet).to_s
+	end
+
+	def clean_match(match)
+		match = /seaport/i.match(match).to_s if match == ""
+				
+		match.gsub!("(", "")
+		match.gsub!(")", "")
+		match.gsub!("at ", "")
+		match.gsub!(" on ", "")
+		match.gsub!("@", "")
+
+		return false if match == ""
+		match
+	end
+
+	def geocode_coordinates(location)
+		return false if location == false
+
+		geo_data = Geocoder.search(location + ", new york city").first
+		latitude = geo_data.geometry["location"]["lat"]
+		longitude = geo_data.geometry["location"]["lng"]
+
+		return false if [latitude, longitude] == [40.7127837, -74.0059413]
+			
+		self.latitude = latitude
+		self.longitude = longitude
+
+		[latitude, longitude].join(",")
+	end
+
+	def get_coordinates(tweet_body)
+		match = parse_tweet(tweet_body)
+		cleaned_match = clean_match(match)
+		coordinates = geocode_coordinates(cleaned_match)
+	end
+
 
 end
--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,4 +1,6 @@
 class Truck < ActiveRecord::Base
+	include LocationHunter
+
 	has_many :tweets
 	has_many :issues
 
@@ -42,43 +44,6 @@ class Truck < ActiveRecord::Base
 		else
 			true
 		end
-	end
-
-
-	def parse_tweet(tweet)
-		my_match = /(@|at|on)\s+((?:\S+\s)?\S*(and|&)\S*(?:\s\S+)?)|\S\d+\s\b\w+\b\s(Avenue|Ave|Street|St)|\A?^?\d+\s(\b\w+\b\s)+(Avenue|Ave|Street|St)|(\b\w+\b\s){2}Park|(\b\w+\b\s)(St|Street)\sand?\s(\b\w+\b\s)(St|Street)/i.match(tweet).to_s
-	end
-
-	def clean_match(match)
-		match = parse_tweet(match)
-
-		match = /seaport/i.match(match).to_s if match == ""
-				
-		match.gsub!("(", "")
-		match.gsub!(")", "")
-		match.gsub!("at ", "")
-		match.gsub!(" on ", "")
-		match.gsub!("@", "")
-
-		return false if match == ""
-		match
-	end
-
-	def geocode_coordinates(location)
-		location = clean_match(location)
-
-		return false if location == false
-
-		geo_data = Geocoder.search(location + ", new york city").first
-		latitude = geo_data.geometry["location"]["lat"]
-		longitude = geo_data.geometry["location"]["lng"]
-
-		return false if [latitude, longitude] == [40.7127837, -74.0059413]
-			
-		self.latitude = latitude
-		self.longitude = longitude
-
-		[latitude, longitude].join(",")
 	end
 
 

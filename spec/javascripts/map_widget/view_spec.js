6bc13f1ff2e68be805d2dc98b22331ba227a862e
Insung Lee <inslee0921@gmail.com>
2014-06-15 14:20:43 -0400

    included ajax helper for jasmine

---
+++ spec/javascripts/map_widget/view_spec.js
@@ -0,0 +1,11 @@
+describe("View", function() {
+  it("should be defined", function() {
+    expect(MapWidget.View).toBeDefined();
+  });
+
+  it("and so is a spec", function() {
+    a = true;
+
+    expect(a).toBe(true);
+  });
+});

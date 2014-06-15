6cfbde9e79751241bf533c4449860e3d32aa916c
niravchheda <niravchheda@gmail.com>
2014-06-15 18:09:59 -0400

    added form for adding new truck

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,11 +43,4 @@ Rails.application.configure do
     config.consumer_secret = ENV['CONSUMER_SECRET']
   end
 
-  client = Twitter::Streaming::Client.new do |config|
-    config.consumer_key        = "YOUR_CONSUMER_KEY"
-    config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-    config.access_token        = "YOUR_ACCESS_TOKEN"
-    config.access_token_secret = "YOUR_ACCESS_SECRET"
-  end
-
 end

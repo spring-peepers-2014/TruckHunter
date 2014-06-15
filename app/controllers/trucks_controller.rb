7f7348778bc31f9ff42734ee2e619c90e21c5223
Bev <beverlymah@gmail.com>
2014-06-15 18:58:18 -0400

    fixing searchbar display

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -2,24 +2,24 @@ class TrucksController < ApplicationController
 	respond_to :json
 	before_filter :load_truck, :except => [:index, :new, :create]
 
-  def index
-  	@trucks = Truck.where(approved: true, active: true)
-  	@current_trucks = @trucks.select { |truck| truck.has_current_location? }
+  # def index
+  # 	@trucks = Truck.where(approved: true, active: true)
+  # 	@current_trucks = @trucks.select { |truck| truck.has_current_location? }
 
-  	@unknown_trucks = @trucks - @current_trucks
+  # 	@unknown_trucks = @trucks - @current_trucks
 
-  	@unknown_trucks.each do |truck|
+  # 	@unknown_trucks.each do |truck|
 
-  		if truck.tweets_last_fetched.nil?
-  			time_since_last_tweet = 9000
-  		else
-  			time_since_last_tweet = Time.now - truck.tweets_last_fetched
-  		end
+  # 		if truck.tweets_last_fetched.nil?
+  # 			time_since_last_tweet = 9000
+  # 		else
+  # 			time_since_last_tweet = Time.now - truck.tweets_last_fetched
+  # 		end
 
-  		truck.fetch_tweets! if time_since_last_tweet > 3600
-    end
-		@updated_trucks = @unknown_trucks.select { |truck| truck.has_current_location? }
-  end
+  # 		truck.fetch_tweets! if time_since_last_tweet > 3600
+  #   end
+		# @updated_trucks = @unknown_trucks.select { |truck| truck.has_current_location? }
+  # end
 
   def new
   	render :json => Truck.geo_json

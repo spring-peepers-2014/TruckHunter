098524dbb1561727b162c67d3fcb745bcef56eb4
Bev <beverlymah@gmail.com>
2014-06-16 09:43:58 -0400

    playing around with understanding pins on map

--- app/assets/javascripts/map_widget/view.js
+++ app/assets/javascripts/map_widget/view.js
@@ -1,5 +1,6 @@
 MapWidget.View = function() {
 	this.map = L.mapbox.map('map', 'inslee.igapaca7').setView([40.75, -73.97], 13);
+	this.currentTrucks = '';
 
 };
 
@@ -7,13 +8,18 @@ MapWidget.View.prototype = {
 
 	draw: function() {
 		var mapLoad = this.map;
-
+		var self = this
 		$.ajax({
     type: "get",
     url: "/trucks/new.json",
     dataType: 'json'
 		}).done(function(response){
 				L.mapbox.featureLayer(response).addTo(mapLoad);
+				console.log(response)
+				self.currentTrucks = response
+				console.log("inside")
+				console.log(self.currentTrucks)
+				console.log(self.currentTrucks[0].properties.title)
 			});
 	}
 };

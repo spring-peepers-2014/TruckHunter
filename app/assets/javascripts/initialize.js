fd6adf24097cf9280860dcbaa072cd00365a6f58
Bev <beverlymah@gmail.com>
2014-06-16 17:18:55 -0400

    working on rendering view when searching

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -1,64 +1,33 @@
 $(document).ready(function(){
 
-	var map = new MapWidget.View();
-	map.draw();
+	var mapView = new MapWidget.View();
+	// mapView.draw();
+  // mapView.grabMarkers();
+  // console.log(mapView)
+  // console.log(mapView.currentTrucks)
 
-  console.log('searching')
-  map.searchMarkersOnMap("Shortys NYC");
+
+
 
 
 
 //on form submit, grab the truck that the user is searching for
-  $('#searchform').on('submit', function(e){
-      e.preventDefault();
-      var searchString = $('input[name="foodtruck"]').val();
-      // searchDatabase(searchString);
-      // console.log("inside form")
-      // console.log(map.currentTrucks)
-      // console.log("what is feature layer")
-      // console.log(map.map.featureLayer)
-      console.log("press enter!")
-      searchMarkersOnMap(searchString);
-      // if (searchMarkersOnMap(searchString)){
-      //   console.log("this is true")
-      //   var truckMarker = grabMarker(searchString);
-      //   console.log("what is truckmarker")
-      //   console.log(truckMarker)
-      //   map.redraw(truckMarker);
-      // }
-    })
-
-
- // function searchMarkersOnMap(searchString) {
- //    console.log('inside')
- //    for (var i=0; i < map.currentTrucks.length; i++) {
- //      // if (map.currentTrucks[i].properties.name === searchString) {
- //      //   return true;
- //      // }
- //      // else {
- //      //   return false;
- //      //   }
- //      console.log('inside for loop')
- //      console.log(map.currentTrucks)
- //    }
- //  };
-
-
-  function grabMarker(searchString) {
-    for (var i=0; i< map.currentTrucks.length ; i++){
-      if (map.currentTrucks[i].properties.name == searchString) {
-        return map.currentTrucks[i];
-      }
-    }
+$('#searchform').on('submit', function(e){
+  e.preventDefault();
+  var searchString = $('input[name="foodtruck"]').val();
+  console.log("press enter!");
+  if (mapView.searchMarkersOnMap(searchString)) {
+    console.log('yay');
+    mapView.redraw(searchString);
   }
+});
 
 
 
-
-  	
-	$('.newtrucktab').on('click', function(){
-		$('.opened').show();
-	});
+$('.newtrucktab').on('click', function(){
+  $('.opened').show();
+});
 
 
 });
+
--- app/assets/javascripts/map_widget/view.js
+++ app/assets/javascripts/map_widget/view.js
@@ -1,36 +1,12 @@
 MapWidget.View = function() {
 	this.map = L.mapbox.map('map', 'inslee.igapaca7').setView([40.75, -73.97], 13);
 	this.layer = L.mapbox.featureLayer().addTo(this.map);
-	this.currentTrucks = [];
+	this.currentTrucks = [];	
+	this.grabMarkers();
+	this.draw();
 };
 
 MapWidget.View.prototype = {
-
-
-	// draw: function() {
-	// 	var mapLoad = this.map;
-	// 	var self = this
-	// 	$.ajax({
- //    type: "get",
- //    url: "/trucks/new.json",
- //    dataType: 'json'
-	// 	}).done(function(response){
-	// 			L.mapbox.featureLayer(response).addTo(mapLoad);
-				
-	// 			for (var i =0; i < response.length; i++){
-	// 				self.currentTrucks.push(response[i])
-	// 			}
-
-	// 			// console.log(response)
-	// 			// self.currentTrucks = response;
-	// 			// console.log("inside")
-	// 			// console.log(self.currentTrucks)
-	// 			// var coordinates = self.currentTrucks[0].geometry.coordinates
-	// 			// console.log(coordinates[0])
-	// 			// console.log(coordinates[1])
-	// 		});
-
-
 	draw: function() {
 		this.layer.loadURL('/trucks/new.json');
 		this.userLocator();
@@ -46,10 +22,10 @@ MapWidget.View.prototype = {
 			geolocate.innerHTML = 'Geolocation is not available';
 		} else {
 			geolocate.onclick = function (e) {
-			e.preventDefault();
-			e.stopPropagation();
-			map.locate();
-};
+				e.preventDefault();
+				e.stopPropagation();
+				map.locate();
+			};
 		}
 
 		// Once we've got a position, zoom and center the map
@@ -57,44 +33,64 @@ MapWidget.View.prototype = {
 		map.on('locationfound', function(e) {
 			map.fitBounds(e.bounds);
 
-      myLayer.setGeoJSON({
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [e.latlng.lng, e.latlng.lat]
-        },
-        properties: {
-            'title': 'Here I am!',
-            'marker-color': '#ff8888',
-            'marker-symbol': 'star'
-        }
-      });
+			myLayer.setGeoJSON({
+				type: 'Feature',
+				geometry: {
+					type: 'Point',
+					coordinates: [e.latlng.lng, e.latlng.lat]
+				},
+				properties: {
+					'title': 'Here I am!',
+					'marker-color': '#ff8888',
+					'marker-symbol': 'star'
+				}
+			});
 // And hide the geolocation button
-			geolocate.parentNode.removeChild(geolocate);
-		});
+geolocate.parentNode.removeChild(geolocate);
+});
 
 		// If the user chooses not to allow their location
 		// to be shared, display an error message.
 		map.on('locationerror', function() {
 			geolocate.innerHTML = 'Position could not be found';
 		});
+	},
+
+	grabMarkers: function() {
+		var self = this;
+		$.ajax({
+			type: "get",
+			url: "/trucks/new.json",
+			dataType: 'json'
+		}).done(function(response){
+				// console.log(response)
+				for (var i =0; i < response.length; i++) {
+					self.currentTrucks.push(response[i]);
+				}
+			});
+	},
+
+	searchMarkersOnMap: function(searchString) {
+		for (var i=0; i < this.currentTrucks.length; i++) {
+			if (this.currentTrucks[i].properties.title == searchString) {
+				return true;
+			}
+		}
+		return false
+	},
+
+	redraw: function(searchString) {
+		console.log('inside redraw')
+		for (var i=0; i < this.currentTrucks.length; i++) {
+			console.log('inside for loop')
+			if (this.currentTrucks[i].properties.title == searchString) {
+				var newCoordinates = this.currentTrucks[i].geometry.coordinates;
+				var coordOne = newCoordinates[0];
+				var coordTwo = newCoordinates[1];
+				this.map.setView([coordTwo, coordOne]);
+			}
+		}
 	}
-
-	// searchMarkersOnMap: function(searchString) {
-	// 	console.log('inside searchMarkerMap function')
-	// 	console.log(this)
-	// 	console.log(this.map)
-	// 	console.log(this.currentTrucks.call())
-	// 	console.log(this.draw)
-	//     for (var i=0; i < this.currentTrucks.length; i++) {
-	//       console.log('inside for loop')
-	//       console.log(this.currentTrucks)
-	//     }
- //  	}
-
-
-
-
 
 
 };

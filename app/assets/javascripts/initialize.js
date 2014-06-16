74b1e929ed8bd096b0ca1d133140d9322d90dbdf
Bev <beverlymah@gmail.com>
2014-06-16 14:14:50 -0400

    playing around with javascript

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -3,7 +3,59 @@ $(document).ready(function(){
 	var map = new MapWidget.View();
 	map.draw();
 
+  console.log('searching')
+  map.searchMarkersOnMap("Shortys NYC");
 
+
+
+//on form submit, grab the truck that the user is searching for
+  $('#searchform').on('submit', function(e){
+      e.preventDefault();
+      var searchString = $('input[name="foodtruck"]').val();
+      // searchDatabase(searchString);
+      // console.log("inside form")
+      // console.log(map.currentTrucks)
+      // console.log("what is feature layer")
+      // console.log(map.map.featureLayer)
+      console.log("press enter!")
+      searchMarkersOnMap(searchString);
+      // if (searchMarkersOnMap(searchString)){
+      //   console.log("this is true")
+      //   var truckMarker = grabMarker(searchString);
+      //   console.log("what is truckmarker")
+      //   console.log(truckMarker)
+      //   map.redraw(truckMarker);
+      // }
+    })
+
+
+ // function searchMarkersOnMap(searchString) {
+ //    console.log('inside')
+ //    for (var i=0; i < map.currentTrucks.length; i++) {
+ //      // if (map.currentTrucks[i].properties.name === searchString) {
+ //      //   return true;
+ //      // }
+ //      // else {
+ //      //   return false;
+ //      //   }
+ //      console.log('inside for loop')
+ //      console.log(map.currentTrucks)
+ //    }
+ //  };
+
+
+  function grabMarker(searchString) {
+    for (var i=0; i< map.currentTrucks.length ; i++){
+      if (map.currentTrucks[i].properties.name == searchString) {
+        return map.currentTrucks[i];
+      }
+    }
+  }
+
+
+
+
+  	
 	$('.newtrucktab').on('click', function(){
 		$('.opened').show();
 	});
--- app/assets/javascripts/map_widget/view.js
+++ app/assets/javascripts/map_widget/view.js
@@ -1,25 +1,55 @@
 MapWidget.View = function() {
 	this.map = L.mapbox.map('map', 'inslee.igapaca7').setView([40.75, -73.97], 13);
-	this.currentTrucks = '';
-
+	this.layer = L.mapbox.featureLayer().addTo(this.map);
+	this.currentTrucks = [];
 };
 
 MapWidget.View.prototype = {
 
+
+	// draw: function() {
+	// 	var mapLoad = this.map;
+	// 	var self = this
+	// 	$.ajax({
+ //    type: "get",
+ //    url: "/trucks/new.json",
+ //    dataType: 'json'
+	// 	}).done(function(response){
+	// 			L.mapbox.featureLayer(response).addTo(mapLoad);
+				
+	// 			for (var i =0; i < response.length; i++){
+	// 				self.currentTrucks.push(response[i])
+	// 			}
+
+	// 			// console.log(response)
+	// 			// self.currentTrucks = response;
+	// 			// console.log("inside")
+	// 			// console.log(self.currentTrucks)
+	// 			// var coordinates = self.currentTrucks[0].geometry.coordinates
+	// 			// console.log(coordinates[0])
+	// 			// console.log(coordinates[1])
+	// 		});
+
+
 	draw: function() {
+		this.layer.loadURL('/trucks/new.json');
+	},
+
+	redraw: function(truckMarker) {
 		var mapLoad = this.map;
-		var self = this
-		$.ajax({
-    type: "get",
-    url: "/trucks/new.json",
-    dataType: 'json'
-		}).done(function(response){
-				L.mapbox.featureLayer(response).addTo(mapLoad);
-				console.log(response)
-				self.currentTrucks = response
-				console.log("inside")
-				console.log(self.currentTrucks)
-				console.log(self.currentTrucks[0].properties.title)
-			});
+		var coordinates = truckMarker.geometry.coordinates;
+		mapLoad.setView([coordinates[0], coordinates[1]]);
 	}
+
+	// searchMarkersOnMap: function(searchString) {
+	// 	console.log('inside searchMarkerMap function')
+	// 	console.log(this)
+	// 	console.log(this.map)
+	// 	console.log(this.currentTrucks.call())
+	// 	console.log(this.draw)
+	//     for (var i=0; i < this.currentTrucks.length; i++) {
+	//       console.log('inside for loop')
+	//       console.log(this.currentTrucks)
+	//     }
+ //  	}
 };

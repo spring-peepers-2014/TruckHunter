8ab9a193d0d2690e3ea7eb996158db93b7922c77
niravchheda <niravchheda@gmail.com>
2014-06-17 11:36:19 -0400

    regex seaport logic

--- app/models/location_hunter.rb
+++ app/models/location_hunter.rb
@@ -3,11 +3,12 @@ module LocationHunter
 	def parse_tweet(tweet)
 		cleaned_tweet = tweet.gsub("#", "")
 		cleaned_tweet.gsub!("&amp;", "&")
+		seaport = /theseaport/i.match(cleaned_tweet).to_s
+		return seaport unless seaport == ""
 		my_match = /((\d+(th|st|nd|rd)\s)|(\S+))\s((ave|avenue|st|street).?\s(b\/t|btw|btwn|between).?\s)(\d+(th|st|nd|rd)|\S+)|\S+\s?(and|&)\s?\S+|\S+\s(and|&)?\s\S+?\s(Avenue|Ave|Street|St)|(\s)?(@|(\s)at|on)\s+((?:\S+\s)?\S*(and|&)\S*(?:\s\S+)?)|\S\d+\s\b\w+\b\s(Avenue|Ave|Street|St)|\A?^?\d+\s(\b\w+\b\s)+(Avenue|Ave|Street|St)|(\b\w+\b\s){2}Park|(\b\w+\b\s)(Avenue|Ave|Street|St)\sand?\s(\b\w+\b\s)(St|Street)|(\b\w+\b\s)between(\s\b\w+\b)/i.match(cleaned_tweet).to_s
 	end
 
 	def clean_match(match)
-		match = /seaport/i.match(match).to_s if match == ""
 
 		match.gsub!("(", "")
 		match.gsub!(")", "")
@@ -15,16 +16,16 @@ module LocationHunter
 		match.gsub!(" on ", "")
 		match.gsub!("@", "")
 		match.gsub!("between", "and")
-		match.gsub!("btw", "and")
-		match.gsub!("btw.", "and")
-		match.gsub!("b/t", "and")
 		match.gsub!("btwn", "and")
-		match.gsub!(/\s?sandwich\s?/, "")
-		match.gsub!(/\s?breakfast\s?/, "")
-		match.gsub!(/\s?lunch\s?/, "")
-		match.gsub!(/\s?dinner\s?/, "")
-		match.gsub!(/\s?down\s?/, "")
-		match.gsub!(/\s?try\s?/, "")
+		match.gsub!("btw.", "and")
+		match.gsub!("btw", "and")
+		match.gsub!("b/t", "and")
+		match.gsub!(/\s?sandwich\s?/i, "")
+		match.gsub!(/\s?breakfast\s?/i, "")
+		match.gsub!(/\s?lunch\s?/i, "")
+		match.gsub!(/\s?dinner\s?/i, "")
+		match.gsub!(/\s?down\s?/i, "")
+		match.gsub!(/\s?try\s?/i, "")
 
 		return false if match == ""
 		match

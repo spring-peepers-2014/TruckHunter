9f96c813b13377d7c0dc35ee58b5c1b1913f52d4
niravchheda <niravchheda@gmail.com>
2014-06-17 18:13:19 -0400

    Added another regex match case Two rspec location_hunter tests

--- spec/models/location_hunter_spec.rb
+++ spec/models/location_hunter_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
 
 describe LocationHunter do
-	context "validations" do
-		it "" do
+	context "regular expressions matching" do
+		it "return correct cross-streets from short-string" do
+			expect( LocationHunter.get_coordinates("40th and 5th") ).to eq("40th and 5th")
 		end
 
-	end
-
-	context "associations" do
-		it "" do
+		it "return the correct cross-streets from tweet" do
+			expect( LocationHunter.get_coordinates("We got kicked out of 50th. Now on 45th and 6th. Come get your taco fix....no line!") ).to eq("45th and 6th.")
 		end
 
-	end
 
+		it "return the correct cross-streets from tweet" do
+			expect( LocationHunter.get_coordinates("We got kicked out of 50th. Now on 45th and 6th. Come get your taco fix....no line!") ).to eq("45th and 6th.")
+		end
+
+		
+
+
+	end
 end

652a77b5496e3be5559c522e732fab20de653011
Insung Lee <inslee0921@gmail.com>
2014-06-17 17:34:48 -0400

    loading image is working

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -1,30 +1,54 @@
 $(document).ready(function(){
+  var map = L.mapbox.map('map').setView([40.75, -73.97], 13);
+	
+  var loader = document.getElementById('loader');
+  
+  startLoading();
 
-	var mapView = new MapWidget.View();
+  L.mapbox.tileLayer('inslee.igapaca7')
+      .addTo(map) // add your tiles to the map
+      .on('load', finishedLoading); // when the tiles load, remove the screen
+
+  var mapView = new MapWidget.View(map);
+
   var mapController = new MapWidget.Controller(mapView);
 
-
-  var searchBarController = new SearchBarWidget.Controller(mapView)
+  var searchBarController = new SearchBarWidget.Controller(mapView);
 
   var tabController = new TabBarWidget.Controller();
 
 
+  function startLoading() {
+      loader.className = '';
+  }
 
-$('form.new_truck').submit(function(e) {
-  e.preventDefault();
+  function finishedLoading() {
+      // first, toggle the class 'done', which makes the loading screen
+      // fade out
+      loader.className = 'done';
+      setTimeout(function() {
+          // then, after a half-second, add the class 'hide', which hides
+          // it completely and ensures that the user can interact with the
+          // map again.
+          loader.className = 'hide';
+      }, 500);
+  }
 
-  $.ajax({
-    type: 'POST',
-    url: '/addtruck',
-    data: $(this).serialize()
-  }).done(function() {
-   // document.getElementById("new_truck").reset();
-   $('#newtruckform form')[0].reset();
-   $('#newtruckform').hide();
-   // console.log(response)
- })
-})
 
+  $('form.new_truck').submit(function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      type: 'POST',
+      url: '/addtruck',
+      data: $(this).serialize()
+    }).done(function() {
+     // document.getElementById("new_truck").reset();
+     $('#newtruckform form')[0].reset();
+     $('#newtruckform').hide();
+     // console.log(response)
+   })
+  })
 
 
 });

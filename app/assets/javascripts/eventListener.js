851162ccddef93ebf69ed5a2970901cb4e056928
Bev <beverlymah@gmail.com>
2014-06-19 09:39:00 -0400

    add new function to event listeners to close all popups

--- app/assets/javascripts/eventListener.js
+++ app/assets/javascripts/eventListener.js
@@ -4,3 +4,58 @@ EventListener = function(view) {
 	this.listenToClose();
 }
 
+
+
+
+EventListener.prototype = {
+	listenToClick: function() {
+		var self = this;
+		$('#addtruck').on('click', function(){
+			self.view.closePopUp();
+			self.view.closeEverything();
+  			$('#usersubmit').show();
+  			$('#truckowner').show();
+		});
+
+		$('#usersubmit').on('click', function(){
+			self.view.closePopUp();
+  			self.view.closeEverything();
+  			$('#usersubmitform').slideDown('slow');
+		});
+
+		$('#truckowner').on('click', function(){
+			self.view.closePopUp();
+  			self.view.closeEverything();
+  			$('#truckownerreadme').slideDown('slow');
+		});
+
+		$('findme').on('click', function() {
+			self.view.closeEverything();
+			console.log('yo')
+		});
+
+		$('form.new_truck').submit(function(e) {
+		    e.preventDefault();
+
+		    $.ajax({
+		      type: 'POST',
+		      url: '/addtruck',
+		      data: $(this).serialize()
+		    }).done(function() {
+		     document.getElementById("new_truck").reset();
+		     $('#usersubmitform').hide();
+		   })
+  		})
+ 	},
+  
+	listenToClose: function() {
+		$(document).on('click', '#close', function(e){
+  			$('#popup').remove();
+  			$('#readme').hide();
+  			$('#newtruckform').hide();
+  			$('#usersubmitform').hide();
+  			$('#truckownerreadme').hide();
+		})
+	}
+}
+
